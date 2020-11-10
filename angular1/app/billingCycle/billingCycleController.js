d248195fc9c3387fc42178e0bb218dda7f4355f3
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-11-10 18:42:38 -0300

    billingcycle-list

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -9,12 +9,21 @@
     const vm = this
     const url = 'http://localhost:3003/api/billingCycles'
 
+    vm.refresh = function() {
+      $http.get(url).then(function(response) {
+        vm.billingCycle = {}
+        vm.billingCycles = response.data
+      })
+    }
+
     vm.create = function () {
       $http.post(url, vm.billingCycle).then(function(response) {
+        vm.refresh
         msgs.addSuccess('Operação realizada com sucesso!')
       }).catch(function(response) {
         msgs.addError(response.data.errors)
       })
     }
+    vm.refresh()
   }
 })()
